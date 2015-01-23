b58632b93ad0d73826351dab3c2aab8883028bbc
jrocharodrigues <jowood09@gmail.com>
2015-01-23 19:40:21 +0000

    coiso

--- app/src/main/java/com/impecabel/photoup/Utils.java
+++ app/src/main/java/com/impecabel/photoup/Utils.java
@@ -4,6 +4,6 @@ package com.impecabel.photoup;
  * Created by jrocharodrigues on 06-01-2015.
  */
 public class Utils {
-    public static final String serverURL = "http://blackbox.impecabel.com/fileReceiver.php";
+    public static final String serverURL = "http://<your-server.com>/fileReceiver.php";
     public static final String parameterName = "uploaded_file[]";
 }
